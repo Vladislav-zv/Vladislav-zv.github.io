e88ee120ecfab6f222c3447f1a992b618479b7e6
Vladyslav <vlzgurskiy2005@gmail.com>
2022-10-21 17:05:21 -0700

    finished the tenth task

---
+++ zd.10/js/script.js
@@ -0,0 +1,6 @@
+function foo()
+{
+    let m_Zgh = parseInt(prompt("Enter number:", ""));
+    document.write(`<p class="text" style="margin: 2% 2% 2% 2%;">1/${m_Zgh} = 1/${m_Zgh+1} + 1/${m_Zgh*(m_Zgh+1)}</p>`);
+}
+foo();
