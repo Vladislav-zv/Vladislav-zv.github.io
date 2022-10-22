b2e613b8048b10b52f8cc2b0a5dc93bfa78a8560
Vladyslav <vlzgurskiy2005@gmail.com>
2022-10-22 09:06:34 -0700

    finished the thith task

--- zd.11/js/script.js
+++ zd.11/js/script.js
@@ -1,6 +1,6 @@
 function foo()
 {
     let m_Zgh = parseInt(prompt("Enter number:", ""));
-    document.write(`<p class="text" style="margin: 2% 2% 2% 2%;">1/${m_Zgh} = 1/${m_Zgh+2} + 
+    document.write(`<p style="margin: 2% 2% 2% 2%; font-size:2vw; color: darkblue">1/${m_Zgh} = 1/${m_Zgh+2} + 
     1/${(m_Zgh+2)*(m_Zgh+3)} + 1/${m_Zgh*(m_Zgh+1)}</p>`);
 }
--- zd.12/js/script.js
+++ zd.12/js/script.js
@@ -1,5 +1,6 @@
 function foo()
 {
     let m_Zgh = parseInt(prompt("Enter number:", ""));
-    document.write(`<p style="margin: 2% 2% 2% 2%;">1/${m_Zgh} = 1/${m_Zgh*2} + 1/${m_Zgh*2}</p>`);
+    document.write(`<p style="margin: 2% 2% 2% 2%; font-size:2vw; color:blue;">1/${m_Zgh} = 1/${m_Zgh*2} + 
+    1/${m_Zgh*2}</p>`);
 }
