e07d0c0e2edfc9d17361167b9fa966eb7b0c07f0
Vladyslav <vlzgurskiy2005@gmail.com>
2022-10-22 09:11:23 -0700

    finished of 13 tasks

--- zd.13/js/script.js
+++ zd.13/js/script.js
@@ -6,7 +6,7 @@ function calc(){
 
     document.write(`<p style="font-size:2vw;">Wyczytane liczby: A = ${numberOne}, B = ${numberTwo}</p><br/></br>`);
 
-    document.write("<p style='color: rgb(0, 0, 128); font-size:3vw;'>");
+    document.write("<p style='color: rgb(0, 0, 128); font-size:2.5vw;'>");
     document.write(`(${numberOne} + ${numberTwo})^3 = ${numberOne}^3 + 3 * ((${numberOne})^2)*(${numberTwo})
      + 3 * (${numberOne})*((${numberTwo})^2) + ${numberTwo}^3 = ${Math.pow(numberOne, 3)} + 
      ${3*Math.pow(numberOne, 2)*numberTwo} + ${3*numberOne*Math.pow(numberTwo, 2)} + ${Math.pow(numberTwo, 3)} 
